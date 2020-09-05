a7eb695375003b17184a327a2e136b572929e2f3
arjeriaharsh <arjeria.harsh1@gmail.com>
2020-09-06 01:09:50 +0530

    Create PalindromeInt.java

---
+++ PalindromeInt.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; 
+
+public class PalindromeInt{
+
+     public static void main(String []args){
+         
+        int num = 1221;
+        int temp = num;
+        int rev = 0, rem;
+        
+        while(temp != 0 ){
+            
+            rem = temp%10;
+            rev = rev*10+rem;
+            temp = temp/10;
+        }
+        
+        if (num == rev){
+            System.out.println(num + " Yes");
+        }else{
+            System.out.println(num + " No");
+        }
+        
+    
+     }
+}
